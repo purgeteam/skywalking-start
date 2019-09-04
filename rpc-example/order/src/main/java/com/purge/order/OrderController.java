8cd34a6d355ff95213633b2438fc9e6e818117b8
Purgeyao <zhaoy@deepblueai.com>
2019-09-04 09:57:09 +0800

    upd

--- rpc-example/order/src/main/java/com/purge/order/OrderController.java
+++ rpc-example/order/src/main/java/com/purge/order/OrderController.java
@@ -1,5 +1,9 @@
 package com.purge.order;
 
+import java.io.FileNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @GetMapping("order")
-    public String order(){
+    public String order(HttpServletRequest request) throws FileNotFoundException {
+        String property = System.getProperty("user.dir");
+        String path1 = ClassUtils.getDefaultClassLoader().getResource("").getPath();
+
+        String path2 = ResourceUtils.getURL("classpath:").getPath();
+        String path = request.getServletContext().getRealPath("");
         return "order";
     }
 }
